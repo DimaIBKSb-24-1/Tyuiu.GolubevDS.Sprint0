839a7ea0429c3a0e0f08baedbdc9d65df0708564
голубев дима <maska.l.v@mail.ru>
2024-09-13 18:12:57 +0500

    Task1

---
+++ Tyuiu.GolubevDS.Sprint0.Task1.V0/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tyuiu.GolubevDS.Sprint0.Task1.V0
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World");
+            string name "Дима";
+            int age = 16;
+            Console.WriteLine("Здраствуйте меня зовут" + name + "мне столько" +age + "лет!");
+            Console.ReadKey();
+        }
+    }
+}
