7a7c0fe0d554cc6fbb4c9d99d543b8c1e84d21d4
голубев дима <maska.l.v@mail.ru>
2024-09-15 22:43:38 +0500

    Task5

---
+++ Tyuiu.GolubevDS.Sprint0.Task5.V0.Test/DataServiceTest.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Tyuiu.GolubevDS.Sprint0.Task5.V0.Lib;
+namespace Tyuiu.GolubevDS.Sprint0.Task5.V0.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+            [TestMethod]
+            public void CheckedAdditionValid()
+            {
+                Assert.AreEqual(10, DataService.Addition(5, 5));
+            }
+            public void CheckedSubtractionValid()
+            {
+                Assert.AreEqual(5, DataService.Subtraction(10, 5));
+            }
+            public void CheckedMultiplicaton()
+            {
+                Assert.AreEqual(50, DataService.Multiplicaton(10, 5));
+            }
+            public void CheckedDivision()
+            {
+                Assert.AreEqual(3, DataService.Division(9, 3));
+            }
+        
+    }
+}
---
+++ Tyuiu.GolubevDS.Sprint0.Task5.V0.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.GolubevDS.Sprint0.Task5.V0.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.GolubevDS.Sprint0.Task5.V0.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("38a41068-6f3d-4cc4-962a-9c162961bcdb")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
